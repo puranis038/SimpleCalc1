09a3956ffc427b5028a2f6edce772fda1d93ab59
Purani <puranis038@gmail.com>
2017-05-06 22:27:37 +0530

    make it better

--- app/index.php
+++ app/index.php
@@ -6,7 +6,7 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script src="../JS/Calculator.js"></script>
+		<script src="../js/calculator.js"></script>
 	</head>
 	<body>
         <div data-ng-app="myApp">
