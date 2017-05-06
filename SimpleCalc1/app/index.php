b9682d9e2ce880af0f66b705cfee830fc31f575f
purani <puranis038@gmail.com>
2017-05-06 22:17:51 +0530

    Add files via upload

--- SimpleCalc1/app/index.php
+++ SimpleCalc1/app/index.php
@@ -2,11 +2,11 @@
 	<head>
 		<title></title>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
-		<link href="../css/style.css" rel="stylesheet" />
+		<link href="style.css" rel="stylesheet" />
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script src="../JS/Calculator.js"></script>
+		<script src="Calculator.js"></script>
 	</head>
 	<body>
         <div data-ng-app="myApp">
