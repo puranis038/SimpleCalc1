86847b25a81dbc952670e3abf408fb49693cdd95
purani <puranis038@gmail.com>
2017-05-06 19:49:31 +0530

    Committing SimpleCalc1

---
+++ SimpleCalc1/JS/Calculator.js
@@ -0,0 +1,15 @@
+﻿angular.module('myApp', []);
+angular.module('myApp').controller('CalcCtrl', function ($scope) {
+    $scope.toggleSimpleInterestResult = false;
+    $scope.toggleCompoundInterestResult = false;
+
+    $scope.simpleCalc = function (principal, rate, time) {
+        $scope.result = parseInt(principal) * (parseInt(rate) / 100) * parseInt(time);
+        $scope.toggleSimpleInterestResult = true;
+    }
+
+    $scope.compoundCalc = function (principal, rate, time) {
+        $scope.result = parseInt(principal) * Math.pow(1 + (parseInt(rate) / 100), parseInt(time));
+        $scope.toggleCompoundInterestResult = true;
+    }
+});
